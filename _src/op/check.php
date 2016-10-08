e4622309748692450e335bcb1492755d385cf253
Illuminati Insider <oranje.db@gmail.com>
2016-10-08 18:34:08 +0300

    Added comments

--- _src/op/check.php
+++ _src/op/check.php
@@ -1,5 +1,13 @@
 <?php
-	
+	/*******************************************
+	function check_int
+	Функция проверяет на наличие переменной и на определенный интервал
+	@param int $var - Собственно число
+	@param int  $lb - Left Border. Крайняя левая граница отрезка
+	@param int $rb - Right Border. Крайняя правая граница отрезка
+	@throws throw403() 
+	@return $var
+	*********************************************/
 	function check_int($var,$lb,$rb) {
 		if ($lb<$rb){
 			if ((isset($var) && isset($rb) && isset($lb))){
@@ -15,7 +23,13 @@
 			
 		}else {throw403("leftborder>rightborder");}
 	}
-	
+	/*******************************************
+	function check_str_eng
+	Функция проверяет строку на соответствие шаблону /[^a-zA-Z0-9_]/, только латинские символы + цифры + _
+	@param string $var - Собственно проверяемая строка
+	@throws throw403() 
+	@return $var
+	*********************************************/
 	function check_str_eng($var) {
 		if(isset($var)){
 			if (preg_match("/[^a-zA-Z0-9_]/", $var)) {
@@ -25,7 +39,13 @@
 			};
 		}
 	}
-	
+	/*******************************************
+	function check_str
+	запрещает все, кроме пробела,_, букв русского и латинского алфавита, цифр
+	@param string $var - Собственно проверяемая строка
+	@throws throw403() 
+	@return $var
+	*********************************************/
 	function check_str($var) {
 		if(isset($var))
 		{
@@ -37,6 +57,15 @@
 		}
 		
 	}	// запрещает все, кроме пробела,_, букв русского и латинского алфавита, цифр
+
+/*******************************************
+	function check_str
+	проверяет на соответствие заданному шаблрну
+	@param string $var - Собственно проверяемая строка
+	@param string $pattern - Собственно шаблон
+	@throws throw403() 
+	@return $var
+	*********************************************/
     function check_str_with_pattern($var,$pattern)
 	{
 		if((isset($var) && isset($pattern)))
@@ -49,7 +78,15 @@
 			};
 		}
 	}
-	
+	/*******************************************
+	function check_str_len
+	Проверяет на соответствие заданной длине, либо между какими-то значениями
+	@param string $var - Собственно проверяемая строка
+	@param string $minlen - Минимальная длина
+	@param string $maxlen - Максимальная длина
+	@throws throw403() 
+	@return $var
+	*********************************************/
 	function check_str_len($var,$minlen,$maxlen){
 		
 		
